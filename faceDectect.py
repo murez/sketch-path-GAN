c8cf429807b825173ce9a6d38b83c72554a01a54
gonggongjohn2 <gonggongjohn@163.com>
2018-07-02 23:48:04 +0800

    Working on faceDetection #1

--- faceDectect.py
+++ faceDectect.py
@@ -1,5 +1,7 @@
 from aip import AipFace
 import base64
+import cv2
+import urllib.parse
 
 APP_ID = '11473153'
 API_KEY = 'If9GTyNGXfyb6GmMjRb48OpX'
@@ -7,20 +9,18 @@ SECRET_KEY = '10AKQg5l5eT5Imf2HzZB4CGsRuXeq7kp'
 
 client = AipFace(APP_ID, API_KEY, SECRET_KEY)
 
-with open("timg.jpg","rb") as f:
-    base64_data = base64.b64encode(f.read())
-    print(base64_data)
-    image = str(base64_data)
-
+f = open('tp.jpg','rb') #二进制方式打开图文件
+ls_f=base64.b64encode(f.read()) #读取文件内容，转换为base64编码
+f.close()
+s=urllib.parse.quote(ls_f)
+print(s)
 imageType = "BASE64"
 
-client.detect(image, imageType);
-
 options = {}
-options["face_field"] = "age"
-options["max_face_num"] = 2
+options["face_field"] = "age,beauty,expression,faceshape,gender,glasses,landmark,race"
+options["max_face_num"] = 1
 options["face_type"] = "LIVE"
 
-result = client.detect(image, imageType, options)
+result = client.detect(s, imageType, options)
 
 print(result)
